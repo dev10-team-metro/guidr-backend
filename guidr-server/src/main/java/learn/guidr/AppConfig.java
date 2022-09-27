c2b2426d653fbb847cac6ae3b19d05ae02f482e0
lindsaypanagia <lpanagia@dev-10.com>
2022-09-27 14:15:49 -0400

    data and test files

--- guidr-server/src/main/java/learn/guidr/AppConfig.java
+++ guidr-server/src/main/java/learn/guidr/AppConfig.java
@@ -22,7 +22,7 @@ public class AppConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://localhost:8080")
+                        .allowedOrigins("http://localhost:3000")
                         .allowedMethods("*");
             }
         };

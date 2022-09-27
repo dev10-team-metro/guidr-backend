17334c1fe13336bddcade811a9e121e34946f625
Eileen Cuevas <eileencuevas62@gmail.com>
2022-09-27 14:48:54 -0400

    git pull changes

--- guidr-server/src/main/java/learn/guidr/controllers/ReviewController.java
+++ guidr-server/src/main/java/learn/guidr/controllers/ReviewController.java
@@ -10,52 +10,52 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/guidr/review")
 public class ReviewController {
-    private final ReviewService service;
-
-    public ReviewController(ReviewService service) {
-        this.service = service;
-    }
-
-    @GetMapping
-    public List<Review> findAll() {
-        return service.findAll();
-    }
-
-    @PostMapping
-    public ResponseEntity<?> create(@RequestBody Review review) {
-        ReviewResult result = service.create(review);
-
-        if (!result.isSuccess()) {
-            return new ResponseEntity<>(result.getErrorMessages(), HttpStatus.BAD_REQUEST);
-        }
-
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<?> update(@PathVariable int id, @RequestBody Review review) {
-        if (id != review.getReviewId()) {
-            return new ResponseEntity<>(HttpStatus.CONFLICT);
-        }
-
-        ReviewResult result = service.update(review);
-        if (!result.isSuccess()) {
-            if (result.getResultType() == ResultType.NOT_FOUND) {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            } else {
-                return new ResponseEntity<>(result.getErrorMessages(), HttpStatus.BAD_REQUEST);
-            }
-        }
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable int id) {
-        ReviewResult result = service.deleteById(id);
-
-        if (result.getResultType() == ResultType.NOT_FOUND) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
+//    private final ReviewService service;
+//
+//    public ReviewController(ReviewService service) {
+//        this.service = service;
+//    }
+//
+//    @GetMapping
+//    public List<Review> findAll() {
+//        return service.findAll();
+//    }
+//
+//    @PostMapping
+//    public ResponseEntity<?> create(@RequestBody Review review) {
+//        ReviewResult result = service.create(review);
+//
+//        if (!result.isSuccess()) {
+//            return new ResponseEntity<>(result.getErrorMessages(), HttpStatus.BAD_REQUEST);
+//        }
+//
+//        return new ResponseEntity<>(HttpStatus.CREATED);
+//    }
+//
+//    @PutMapping("/{id}")
+//    public ResponseEntity<?> update(@PathVariable int id, @RequestBody Review review) {
+//        if (id != review.getReviewId()) {
+//            return new ResponseEntity<>(HttpStatus.CONFLICT);
+//        }
+//
+//        ReviewResult result = service.update(review);
+//        if (!result.isSuccess()) {
+//            if (result.getResultType() == ResultType.NOT_FOUND) {
+//                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//            } else {
+//                return new ResponseEntity<>(result.getErrorMessages(), HttpStatus.BAD_REQUEST);
+//            }
+//        }
+//        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<Void> delete(@PathVariable int id) {
+//        ReviewResult result = service.deleteById(id);
+//
+//        if (result.getResultType() == ResultType.NOT_FOUND) {
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        }
+//        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//    }
 }
--- guidr-server/src/test/java/learn/guidr/controllers/ReviewControllerTest.java
+++ guidr-server/src/test/java/learn/guidr/controllers/ReviewControllerTest.java
@@ -29,162 +29,162 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 class ReviewControllerTest {
-
-    @MockBean
-    ReviewRepository repository;
-
-    @Autowired
-    MockMvc mvc;
-
-    final String api = "/api/guidr/review";
-
-    final Review TEST_REVIEW = new Review(
-            1,
-            "Test Review",
-            new BigDecimal("9.99"),
-            5,
-            new SiteCollection());
-
-    @Test
-    void shouldFindAllReturning200() throws Exception {
-        List<Review> expected = new ArrayList<>();
-        expected.add(new Review());
-        expected.add(new Review());
-
-        when(repository.findAll()).thenReturn(expected);
-
-        mvc.perform(get(api))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().json(objectToJson(expected)));
-    }
-
-    @Test
-    void shouldFindByIdReturning200() throws Exception {
-        Review expected = TEST_REVIEW;
-
-        when(repository.findById(1).thenReturn(expected));
-
-        mvc.perform(get(api + "/1"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().json(objectToJson(expected)));
-    }
-
-    @Test
-    void shouldNotFindMissingReturning404() throws Exception {
-        mvc.perform(get(api + "/99"))
-                .andExpect(status().isNotFound());
-    }
-
-    @Test
-    void shouldAddReturning201() throws Exception {
-        Review review = new Review(
-                0,
-                "Test Review",
-                new BigDecimal("9.99"),
-                5,
-                new SiteCollection());
-
-        Review expected = TEST_REVIEW;
-
-        when(repository.add(any())).thenReturn(expected);
-
-        var request = post(api)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectToJson(review));
-
-        mvc.perform(request)
-                .andExpect(status().isCreated())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().json(objectToJson(expected)));
-    }
-
-    @Test
-    void shouldNotAddReturning400() throws Exception {
-        Review review = TEST_REVIEW;
-
-        var request = post(api)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectToJson(review));
-
-        mvc.perform(request)
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    void shouldUpdateReturning204() throws Exception {
-        when(repository.update(any())).thenReturn(true);
-
-        var request = put(api + "/1")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectToJson(TEST_REVIEW));
-
-        mvc.perform(request)
-                .andExpect(status().isNoContent());
-    }
-
-    @Test
-    void shouldNotUpdateReturning400() throws Exception {
-        Review review = new Review(
-                3,
-                "Test Review",
-                new BigDecimal("9.99"),
-                5,
-                new SiteCollection());
-
-        List<Review> all = new ArrayList<>();
-        all.add(TEST_REVIEW);
-
-        when(repository.findAll()).thenReturn(all);
-
-        var request = put(api + "/3")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectToJson(review));
-
-        mvc.perform(request)
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    void shouldNotUpdateReturning409() throws Exception {
-        var request = put(api + "/3")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectToJson(TEST_REVIEW));
-
-        mvc.perform(request)
-                .andExpect(status().isConflict());
-    }
-
-    @Test
-    void shouldDeleteReturning204() throws Exception {
-        when(repository.deleteById(1)).thenReturn(true);
-        when(repository.findById(1)).thenReturn(TEST_REVIEW);
-
-        mvc.perform(delete(api + "/1"))
-                .andExpect(status().isNoContent());
-    }
-
-    @Test
-    void shouldNotDeleteReturning404() throws Exception {
-        mvc.perform(delete(api + "/2"))
-                .andExpect(status().isNotFound());
-    }
-
-    @Test
-    void shouldNotDeleteReturning400() throws Exception {
-        when(repository.deleteById(1)).thenReturn(false);
-        when(repository.findById(1)).thenReturn(TEST_REVIEW);
-
-        mvc.perform(delete(api + "/1"))
-                .andExpect(status().isBadRequest());
-    }
-
-    private String objectToJson(Object object) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        return objectMapper.writeValueAsString(object);
-    }
+//
+//    @MockBean
+//    ReviewRepository repository;
+//
+//    @Autowired
+//    MockMvc mvc;
+//
+//    final String api = "/api/guidr/review";
+//
+//    final Review TEST_REVIEW = new Review(
+//            1,
+//            "Test Review",
+//            new BigDecimal("9.99"),
+//            5,
+//            new SiteCollection());
+//
+//    @Test
+//    void shouldFindAllReturning200() throws Exception {
+//        List<Review> expected = new ArrayList<>();
+//        expected.add(new Review());
+//        expected.add(new Review());
+//
+//        when(repository.findAll()).thenReturn(expected);
+//
+//        mvc.perform(get(api))
+//                .andExpect(status().isOk())
+//                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+//                .andExpect(content().json(objectToJson(expected)));
+//    }
+//
+//    @Test
+//    void shouldFindByIdReturning200() throws Exception {
+//        Review expected = TEST_REVIEW;
+//
+//        when(repository.findById(1).thenReturn(expected));
+//
+//        mvc.perform(get(api + "/1"))
+//                .andExpect(status().isOk())
+//                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+//                .andExpect(content().json(objectToJson(expected)));
+//    }
+//
+//    @Test
+//    void shouldNotFindMissingReturning404() throws Exception {
+//        mvc.perform(get(api + "/99"))
+//                .andExpect(status().isNotFound());
+//    }
+//
+//    @Test
+//    void shouldAddReturning201() throws Exception {
+//        Review review = new Review(
+//                0,
+//                "Test Review",
+//                new BigDecimal("9.99"),
+//                5,
+//                new SiteCollection());
+//
+//        Review expected = TEST_REVIEW;
+//
+//        when(repository.add(any())).thenReturn(expected);
+//
+//        var request = post(api)
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(objectToJson(review));
+//
+//        mvc.perform(request)
+//                .andExpect(status().isCreated())
+//                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+//                .andExpect(content().json(objectToJson(expected)));
+//    }
+//
+//    @Test
+//    void shouldNotAddReturning400() throws Exception {
+//        Review review = TEST_REVIEW;
+//
+//        var request = post(api)
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(objectToJson(review));
+//
+//        mvc.perform(request)
+//                .andExpect(status().isBadRequest());
+//    }
+//
+//    @Test
+//    void shouldUpdateReturning204() throws Exception {
+//        when(repository.update(any())).thenReturn(true);
+//
+//        var request = put(api + "/1")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(objectToJson(TEST_REVIEW));
+//
+//        mvc.perform(request)
+//                .andExpect(status().isNoContent());
+//    }
+//
+//    @Test
+//    void shouldNotUpdateReturning400() throws Exception {
+//        Review review = new Review(
+//                3,
+//                "Test Review",
+//                new BigDecimal("9.99"),
+//                5,
+//                new SiteCollection());
+//
+//        List<Review> all = new ArrayList<>();
+//        all.add(TEST_REVIEW);
+//
+//        when(repository.findAll()).thenReturn(all);
+//
+//        var request = put(api + "/3")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(objectToJson(review));
+//
+//        mvc.perform(request)
+//                .andExpect(status().isBadRequest());
+//    }
+//
+//    @Test
+//    void shouldNotUpdateReturning409() throws Exception {
+//        var request = put(api + "/3")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(objectToJson(TEST_REVIEW));
+//
+//        mvc.perform(request)
+//                .andExpect(status().isConflict());
+//    }
+//
+//    @Test
+//    void shouldDeleteReturning204() throws Exception {
+//        when(repository.deleteById(1)).thenReturn(true);
+//        when(repository.findById(1)).thenReturn(TEST_REVIEW);
+//
+//        mvc.perform(delete(api + "/1"))
+//                .andExpect(status().isNoContent());
+//    }
+//
+//    @Test
+//    void shouldNotDeleteReturning404() throws Exception {
+//        mvc.perform(delete(api + "/2"))
+//                .andExpect(status().isNotFound());
+//    }
+//
+//    @Test
+//    void shouldNotDeleteReturning400() throws Exception {
+//        when(repository.deleteById(1)).thenReturn(false);
+//        when(repository.findById(1)).thenReturn(TEST_REVIEW);
+//
+//        mvc.perform(delete(api + "/1"))
+//                .andExpect(status().isBadRequest());
+//    }
+//
+//    private String objectToJson(Object object) throws JsonProcessingException {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        objectMapper.registerModule(new JavaTimeModule());
+//        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+//        return objectMapper.writeValueAsString(object);
+//    }
 
 }
